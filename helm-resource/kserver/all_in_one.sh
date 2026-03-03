5d88d353cbc8712b5bdccc36c8e31ec78699644c
ykl <2436542696@qq.com>
2026-03-03 10:37:48 +0800

    添加kserver

---
+++ helm-resource/kserver/all_in_one.sh
@@ -0,0 +1,9 @@
+
+./manage.cert-manager-helm.sh
+./manage.gateway-api-crd.sh
+./manage.envoy-gateway-helm.sh
+./manage.envoy-ai-gateway-helm.sh
+./manage.lws-operator.sh
+./manage.gateway-api-gwclass.sh
+./manage.gateway-api-gw.sh
+./manage.kserve-helm.sh
